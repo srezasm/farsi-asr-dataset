4c981cea42c483cae0d3b5ee0a07e06e511036c5
srezasm <srezas.2017@gmail.com>
2025-02-28 21:32:31 +0330

    update youtube.py

--- subtitles_cleanup/youtube.py
+++ subtitles_cleanup/youtube.py
@@ -7,6 +7,8 @@ from typing import Generator
 import logging
 import time
 import tqdm
+import tarfile
+import shutil
 
 # Clear existing handlers and configure fresh
 logging.root.handlers = []
@@ -23,30 +25,67 @@ logging.root.setLevel(logging.INFO)
 
 class FilesIterator:
     def __init__(self):
-        self.dir_path = '/home/srezas/Programming/projects/negar/samples/UCa-ZgzdNQYDOet7t_yrcPHw'
-        self.files = list(self._get_files())
+        self.api = HfApi()
+        self.repo_id = 'farsi-asr/farsi-asr-dataset'
+        self.tar_files = self._get_tar_files()
+    
+    def _get_tar_files(self):
+        tar_files = self.api.list_repo_files(self.repo_id, repo_type='dataset')
+        tar_files = [f for f in tar_files if f.startswith('youtube') and f.endswith('.tar.gz')]
+        return tar_files
 
-    def _get_files(self) -> Generator[tuple[str, str], None, None]:
-        dir_list = os.listdir(self.dir_path)
-        for sub_dir in dir_list:
-            sub_path = glob.glob(os.path.join(self.dir_path, sub_dir, '*.vtt'))
-            audio_path = glob.glob(os.path.join(
-                self.dir_path, sub_dir, '*.opus'))
+    def _get_files(self):
+        tmp_dir = '/content/tmp'
 
-            if not sub_path or not audio_path:
-                not_found = 'subtitles and audio' if not sub_path and not audio_path else (
-                    'subtitles' if not sub_path else 'audio')
-                logging.error(
-                    f"Could not find {not_found} file for {sub_dir} directory")
-                continue
+        for tar_file in self.tar_files:
+            os.makedirs(tmp_dir, exist_ok=True)
+            
+            print(f'Working on {tar_file}...')
 
-            yield sub_path[0], audio_path[0]
+            # Download the tar file
+            tar_path = self.api.hf_hub_download(
+                self.repo_id, tar_file, repo_type='dataset', local_dir=tmp_dir
+            )
+            
+            # Extract the tar file
+            with tarfile.open(tar_path, 'r:gz') as tar:
+                tar.extractall(tmp_dir)
+            
+            # Yield files from the extracted directory
+            chl_id = os.path.basename(tar_file).split('_')[0]
+            for root, _, files in os.walk(os.path.join(tmp_dir, chl_id)):
+                sub_path = [os.path.join(root, f) for f in files if f.endswith('.vtt')]
+                audio_path = [os.path.join(root, f) for f in files if f.endswith('.opus')]
+                
+                if not sub_path or not audio_path:
+                    not_found = 'subtitles and audio' if not sub_path and not audio_path else (
+                        'subtitles' if not sub_path else 'audio'
+                    )
+                    logging.warning(f"Could not find {not_found} file in {root}. Skipping...")
+                    continue
+                
+                yield sub_path[0], audio_path[0], os.path.basename(tar_file).split('.')[0]
 
-    def __len__(self):
-        return len(self.files)
+            # Clean up the temporary directory for this tar file
+            shutil.rmtree(tmp_dir)
+
+            batch_files = glob.glob(f'/content/{os.path.basename(tar_file).split(".")[0]}/*')
+            # Create new compressed archive with combined content
+            with tarfile.open(os.path.basename(tar_path), 'w:gz') as tar:
+                for fpath in batch_files:
+                    arcname = f'{chl_id}/{os.path.basename(fpath)}'
+                    tar.add(fpath, arcname=arcname)
+                    shutil.rmtree(fpath, ignore_errors=True)
+            
+            self.api.upload_file(
+                path_or_fileobj=os.path.basename(tar_path),
+                path_in_repo=os.path.basename(tar_path),
+                repo_id='farsi-asr/farsi-youtube-asr-dataset',
+                repo_type='dataset'
+            )
 
     def __iter__(self):
-        return iter(self.files)
+        return self._get_files()
 
 
 def get_captions(sub_filepath):
@@ -82,18 +121,20 @@ if __name__ == "__main__":
 
     with get_db_session() as session:
 
-        p_bar = tqdm.tqdm(total=len(video_iter))
-        for sub_filepath, audio_filepath in video_iter:
+        # p_bar = tqdm.tqdm()
+        for sub_filepath, audio_filepath, chl_id in video_iter:
             vid_id = os.path.basename(sub_filepath).split('.')[0]
-            os.makedirs(vid_id, exist_ok=True)
+            print(vid_id)
+            
+            dir_path = os.path.join(chl_id, vid_id)
+            os.makedirs(dir_path, exist_ok=True)
 
             captions = chunker.chunk(
                 merge=True,
                 audio_file=audio_filepath,
                 captions=get_captions(sub_filepath),
-                output_dir=vid_id
+                output_dir=dir_path
             )
 
             create_chunks(session, 'youtube', vid_id, captions)
-
-            p_bar.update(1)
+            # p_bar.update(1)
